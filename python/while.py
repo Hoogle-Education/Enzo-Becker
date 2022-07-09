589d95e63434b2616581b235f08badb964c0bb46
hgrafa <hugorafell@gmail.com>
2022-07-09 15:01:53 -0300

    Adding new python samples about loops

---
+++ python/while.py
@@ -0,0 +1,12 @@
+silksong_lancamento = False
+numero_da_reclamacao = 1
+
+while not silksong_lancamento :
+
+  print(f'lança logo teamcherry #{numero_da_reclamacao}')
+  numero_da_reclamacao += 1
+  
+  if numero_da_reclamacao == 40000 :
+    silksong_lancamento = True
+
+print("Finalmente lançou ...")
